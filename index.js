3587aa47032877bc6935becc5d2c7da5ff816640
KarthikSrishty <karthikeyasrishty@gmail.com>
2025-04-20 23:38:50 +0530

    initial commit

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ app.use("/users", userRouter)
 const URL='mongodb+srv://KarthikeyaSrishty:KarthikeyaSrishty@cluster0.g9kov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(URL,{ dbName: "Nanna" });
-
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
